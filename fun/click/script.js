9a593ebd589fce29c1f89312f0b18ffcc0b46fe0
pudol <94850075+pudolll@users.noreply.github.com>
2024-09-12 06:57:16 +0200

    a

--- fun/click/script.js
+++ fun/click/script.js
@@ -3,6 +3,6 @@ let count = 0;
 function clickCounter() {
   count++;
   document.getElementById('counter').innerHTML = `Counter: ${count}`;
-  document.getElementById('increase').style.transform = 'translate(' + count + 'px,' + count / 2+'px)';
   document.getElementById('counter').style.transform = 'translate(' + count + 'px,' + count * 2+'px)';
+  document.getElementById('increase').style.fontSize = 100 - count + 'px'
 };
--- script.js
+++ script.js
@@ -1,15 +1,28 @@
+let showcase = document.querySelector("#showcase");
 let pudol = document.querySelector("#pudol");
 
-if (pudol) {
+if (showcase && pudol) {
+  // Initial setup for background position
+  updateBackgroundPosition();
 
   function updateAnimation() {
     const d = new Date();
     let time = d.getTime();
 
+    // Update the transform of the pudol element
     pudol.style.transform = `translate(0,${Math.sin(time / 1000) * 20}%) rotate(${(Math.sin(time / 750) * 3 + 1.5)}deg)`;
 
+    // Update the background position of the showcase element
+    updateBackgroundPosition(time);
+
+    // Request the next frame
     requestAnimationFrame(updateAnimation);
   }
 
+  function updateBackgroundPosition(time = 0) {
+    showcase.style.backgroundPositionY = `${(window.scrollY / window.innerHeight) * 30 + 50 + Math.sin(time / 2300) * 5}%`;
+  }
+
+  // Start the animation loop
   requestAnimationFrame(updateAnimation);
 }
